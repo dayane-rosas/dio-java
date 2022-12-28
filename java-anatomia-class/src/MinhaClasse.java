0f947289cc88d66922336edf695e158a0812fe36
danyrosas <dayanerosas@gmail.com>
2022-12-28 12:56:02 -0400

    Initial commit

--- java-anatomia-class/src/MinhaClasse.java
+++ java-anatomia-class/src/MinhaClasse.java
@@ -2,7 +2,7 @@ public class MinhaClasse {
 
     public static void main(String[] args) {
 
-        System.out.print("Olá mundo!");
+        System.out.print("Olá turma, sejam bem-vindos!");
     }
 
 }
