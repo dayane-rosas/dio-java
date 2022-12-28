ef9897bc971da64faff749a4afef6a037329de99
danyrosas <dayanerosas@gmail.com>
2022-12-28 15:16:12 -0400

    Alteracao commit

--- java-anatomia-class/src/edu/dayanerosas/anatomiaclass/BoletimEstudantil.java
+++ java-anatomia-class/src/edu/dayanerosas/anatomiaclass/BoletimEstudantil.java
@@ -1,3 +1,5 @@
+package anatomiaclass;
+
 public class BoletimEstudantil {
 
     public static void main(String[] args) {
--- java-anatomia-class/src/edu/dayanerosas/anatomiaclass/MinhaClasse.java
+++ java-anatomia-class/src/edu/dayanerosas/anatomiaclass/MinhaClasse.java
@@ -1,3 +1,5 @@
+package anatomiaclass;
+
 public class MinhaClasse {
 
     public static void main(String[] args) {
