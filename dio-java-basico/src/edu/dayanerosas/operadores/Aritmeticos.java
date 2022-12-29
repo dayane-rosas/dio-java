a850161575bce43b41efdfcff1ff583f54585e24
danyrosas <dayanerosas@gmail.com>
2022-12-29 18:44:30 -0400

    Atualizacao commit

---
+++ dio-java-basico/src/edu/dayanerosas/operadores/Aritmeticos.java
@@ -0,0 +1,34 @@
+package operadores;
+
+public class Aritmeticos {
+    public static void main(String[] args) {
+        // classe Operadores.java
+        /*
+         * double soma = 10.5 + 15.7;
+         * int subtração = 113 - 25;
+         * int multiplicacao = 20 * 7;
+         * int divisao = 15 / 3;
+         * int modulo = 18 % 3;
+         * double resultado = (10 * 7) + (20 / 4);
+         */
+
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        // qual o resultado das expressoes abaixo?
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        concatenacao = "1" + (1 + 1 + 1);
+
+        System.out.println(concatenacao);
+
+    }
+
+}
---
+++ dio-java-basico/src/edu/dayanerosas/operadores/Atribuicao.java
@@ -0,0 +1,24 @@
+package operadores;
+
+public class Atribuicao {
+
+    public static void main(String[] args) {
+        // classe Operadores.java
+        String nome = "DAYANE";
+        System.out.println(nome);
+
+        int idade = 35;
+        System.out.println(idade);
+
+        double peso = 57.5;
+        System.out.println(peso);
+
+        char sexo = 'F';
+        System.out.println(sexo);
+
+        boolean doadorOrgao = true;
+        System.out.println(doadorOrgao);
+
+        // Date dataNascimento = new Date();
+    }
+}
